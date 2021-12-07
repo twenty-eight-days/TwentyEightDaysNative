f385e034ffe3a3492d3051195e167366d47c2cf9
Jessica Hofmann <jessica.hofmann@gmx.ch>
2021-12-07 14:23:59 +0100

    Löschen einzelner Items unterstützen #8

--- src/model/storage.ts
+++ src/model/storage.ts
@@ -3,6 +3,7 @@ import EncryptedStorage from 'react-native-encrypted-storage'
 export interface Storage {
   read: () => Promise<Period[]>
   write: (period: Period) => Promise<Period[]>
+  delete: (period: Period) => Promise<Period[]>
 }
 
 export interface Period {
@@ -26,9 +27,22 @@ async function getAsyncPeriod() {
   }
 }
 
+async function removeDate(periodToDelete: Period) {
+  const periods = await storage.read()
+  const updatedPeriods = []
+  for (let period of periods) {
+    if (period.date.toDateString() !== periodToDelete.date.toDateString()) {
+      updatedPeriods.push(period)
+    }
+  }
+  await EncryptedStorage.setItem('period', JSON.stringify(updatedPeriods))
+  return await getAsyncPeriod()
+}
+
 export const storage: Storage = {
   read: getAsyncPeriod,
   write: pushAsyncPeriod,
+  delete: removeDate,
 }
 
 console.log(storage.read())
