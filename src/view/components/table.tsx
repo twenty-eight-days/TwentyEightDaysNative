9d7cc21483100e88acf556638e6df7f68ed1fa0c
Marcos Gamez <marcos.gam@hotmail.com>
2021-12-07 14:32:46 +0100

    #15 Fix warning React list JSX has no key

--- src/view/components/table.tsx
+++ src/view/components/table.tsx
@@ -5,8 +5,10 @@ import tailwind from 'tailwind-react-native-classnames'
 export const Table = (props: { table: string[] }) => {
   return (
     <View style={tailwind.style('flex flex-col')}>
-      {props.table.map(item => (
-        <Text style={tailwind.style('py-4')}>{item}</Text>
+      {props.table.map((item, index) => (
+        <Text key={index} style={tailwind.style('py-4')}>
+          {item}
+        </Text>
       ))}
     </View>
   )
