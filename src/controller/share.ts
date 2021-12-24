7c23752c154b7c816c811d230d6a2bec20fb9b47
Marcos <marcos.gamez@students.fhnw.ch>
2021-12-24 01:14:05 +0100

    #24 removed non-working code. Needs to be readdressed later

--- src/controller/share.ts
+++ src/controller/share.ts
@@ -1,38 +1,6 @@
-import { Period, storage } from '../model/storage'
-import { Alert, Share } from 'react-native'
-import ReceiveSharingIntent from 'react-native-receive-sharing-intent'
-import { store, write } from './redux'
+import { Period } from '../model/storage'
+import { Share } from 'react-native'
 
 export function exportJson(periods: Period[]) {
   Share.share({ message: JSON.stringify(periods) })
 }
-ReceiveSharingIntent.getReceivedFiles(
-  (files: [{ text: string }]) => {
-    // files returns as JSON Array example
-    //[{ filePath: null, text: null, weblink: null, mimeType: null, contentUri: null, fileName: null, extension: null }]
-    const periods = JSON.parse(files[0].text) as Period[]
-    for (const period of periods) {
-      storage
-        .write(period)
-        .then(p => {
-          store.dispatch(write(p))
-          Alert.alert('Import successfully')
-        })
-        .catch(() => {
-          Alert.alert(
-            'Import Failed',
-            `We were unable to import the Date ${period.date.toDateString()}`,
-            [{ text: 'OK' }]
-          )
-        })
-    }
-  },
-  (error: Error) => {
-    console.log(error)
-    Alert.alert('Import Failed', 'We were unable to import', [{ text: 'OK' }])
-  },
-  'ShareMedia32345' // share url protocol (must be unique to your app, suggest using your apple bundle id)
-)
-
-// To clear Intents
-ReceiveSharingIntent.clearReceivedFiles()
