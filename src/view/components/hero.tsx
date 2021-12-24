fa21e05eb9584d182ff271a213a7ed63bf650ea0
Marcos <marcos.gamez@students.fhnw.ch>
2021-12-24 01:15:23 +0100

    #24 #21 Add Spacec to increase readability and format today hero screen

--- src/view/components/hero.tsx
+++ src/view/components/hero.tsx
@@ -12,27 +12,34 @@ export const Hero = (props: { periods: Period[] }) => {
   return (
     <View
       style={tailwind.style(
-        'flex flex-col flex-wrap bg-pink-200 p-4 rounded-xl shadow-xl'
+        'flex flex-col flex-wrap bg-pink-200 p-4 rounded-xl shadow'
       )}
     >
       <View style={tailwind.style('flex flex-row')}>
-        <Text style={tailwind.style('text-black text-xs text-gray-500')}>
+        <Text style={tailwind.style('text-black text-xs text-gray-600')}>
+          Today:
+        </Text>
+        <Text
+          style={tailwind.style('text-black ml-auto text-xs text-gray-600')}
+        >
           {currentDate(props.periods).toDateString()}
         </Text>
       </View>
+
       <View style={tailwind.style('flex flex-row')}>
         <Text style={tailwind.style('text-black')}>Next Cycle:</Text>
         <Text style={tailwind.style('text-black ml-auto font-black')}>
           {expectedDate(props.periods).toDateString()}
         </Text>
       </View>
+
       <View style={tailwind.style('flex flex-col')}>
         <Text style={tailwind.style('text-black my-auto')}>
           Current cycle day:
         </Text>
         <Text style={tailwind.style('text-4xl text-black font-black m-auto')}>
           {currentDuration(
-            props.periods.length !== 0 ? props.periods[0].date : new Date()
+            props.periods.length === 0 ? new Date() : props.periods[0].date
           )}
         </Text>
       </View>
--- src/view/containers/Screen.tsx
+++ src/view/containers/Screen.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
 import { SafeAreaView, StatusBar } from 'react-native'
-import { MainScreen } from './MainScreen'
-import tailwind from 'tailwind-react-native-classnames'
-import { Footer } from './Footer'
-import { Header } from './Header'
 import { RootState } from '../../controller/redux'
+import tailwind from 'tailwind-react-native-classnames'
+import { useSelector } from 'react-redux'
+import { TodayScreen } from './TodayScreen'
+import { Footer } from '../components/footer'
+import { Header } from '../components/header'
 import { Popup } from '../components/popup'
 
 export const Screen = () => {
-  const [main, setMain] = useState(<MainScreen />)
-  const popup = useSelector((state: RootState) => state.popup.visible)
+  const [main, setMain] = useState(<TodayScreen />)
+  const popup = useSelector((state: RootState) => state.popup.popupIsVisible)
   return (
     <SafeAreaView style={tailwind.style('text-black h-full bg-pink-50')}>
       {!popup ? null : <Popup />}
       <StatusBar />
+
       <Header />
+
       {main}
+
       <Footer setMain={setMain} />
     </SafeAreaView>
   )
