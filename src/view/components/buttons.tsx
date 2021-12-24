5dd4103d2f053b3a0a5b408420e5c40a4d4945b7
Marcos <marcos.gamez@students.fhnw.ch>
2021-12-24 01:08:33 +0100

    #24 moved datepicker to buttons.tsx

--- src/view/components/buttons.tsx
+++ src/view/components/buttons.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Image, Text, TouchableHighlight, View } from 'react-native'
 import tailwind from 'tailwind-react-native-classnames'
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker'
 
+export const Button = (props: { onPress: (f: any) => void; text: string }) => {
+  return (
+    <TouchableHighlight
+      style={tailwind.style(
+        'bg-yellow-500 rounded text-white w-32 p-2 mr-auto shadow-xl'
+      )}
+      onPress={props.onPress}
+    >
+      <View style={tailwind.style('flex flex-row')}>
+        <Text style={tailwind.style('m-auto text-white text-xl')}>
+          {props.text}
+        </Text>
+      </View>
+    </TouchableHighlight>
+  )
+}
+export const Datepicker = (props: {
+  onPress: (event: Event, d: Date | undefined) => void
+}) => {
+  const [date] = useState(new Date())
+  const [show, setShow] = useState(false)
+  return (
+    <View style={tailwind.style('w-32')}>
+      <ButtonImg
+        onPress={() => setShow(true)}
+        text={'Date'}
+        src={require('../ressources/white_today.png')}
+      />
+      {show && (
+        <DateTimePicker
+          testID="datePicker"
+          value={date}
+          display="default"
+          maximumDate={new Date()}
+          onChange={(event: Event, d: Date | undefined) => {
+            props.onPress(event, d)
+            setShow(false)
+          }}
+        />
+      )}
+    </View>
+  )
+}
 export const ButtonImg = (props: {
   onPress: (f: any) => void
   text: string
@@ -34,6 +78,7 @@ export const ButtonNav = (props: {
 }) => {
   return (
     <TouchableHighlight
+      underlayColor={'#fbf1f7'}
       style={tailwind.style('w-32 p-2 m-auto rounded')}
       onPress={props.onPress}
     >
@@ -44,22 +89,6 @@ export const ButtonNav = (props: {
           source={props.src}
         />
         <Text style={tailwind.style('m-auto text-black text-xl')}>
-          {props.text}
-        </Text>
-      </View>
-    </TouchableHighlight>
-  )
-}
-export const Button = (props: { onPress: (f: any) => void; text: string }) => {
-  return (
-    <TouchableHighlight
-      style={tailwind.style(
-        'bg-yellow-500 rounded text-white w-32 p-2 mr-auto shadow-xl'
-      )}
-      onPress={props.onPress}
-    >
-      <View style={tailwind.style('flex flex-row')}>
-        <Text style={tailwind.style('m-auto text-white text-xl')}>
           {props.text}
         </Text>
       </View>
